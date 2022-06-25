c76b702a7169e75ebb4d9f7c3990ff6c66050fbe
leo4life2 <leo4life@163.com>
2022-06-25 16:57:01 -0400

    first page done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import logo from "./assets/img/Logo.svg"
 import startvote from "./assets/img/startvote.svg"
 import topButton from "./assets/img/topButton.svg"
+import bird from "./assets/img/bird.png"
+import page2 from "./assets/img/page2.svg"
 
 function App(props) {
   const {
@@ -24,13 +26,52 @@ function App(props) {
         <img src={logo} alt="Logo"/>
         <img src={startvote} alt="startvote"/>
       </div>
+
       <div className="content">
         <span className="devoted-to-dem"> Devoted to democracy. </span>
         <span className="subtitle"> Protect the power of your votes from being stolen by fake voters.</span>
         <img className="topButton" src={topButton} />
+        <span className="trendingVote"> Trending Vote </span>
+
+        <div className="voteCard">
+          <img src={bird} className="topVoteImg"/>
+          <div className="rightPart">
+
+            <div className="topCard">
+              <span className="cardTitle"> Vote for protection of wildlife </span>
+              <span className="cardSubtitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, at volutpat arcu, ut odio montes. Sed integer lobortis massa nisi, posuere.
+              </span>
+            </div>
+
+            <div className="bottomCard">
+              <div className="progressBar">
+                <span className="progressTitle"> Unique Voters </span>
+                <span className="progressCount"> 25000 </span>
+                <div className="progressSection"></div>
+              </div>
+
+              <div className="btnContainer">
+                <div className="btn">
+                  <span className="btnText"> 1 </span>
+                </div>
+                <div className="btn">
+                  <span className="btnText"> 2 </span>
+                </div>
+                <div className="btn">
+                  <span className="btnText"> 3 </span>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <img src={page2} className="page2"/>
+
       </div>
-      <span className="trendingVote"> Trending Vote </span>
-      
+
+
     </div>
   );
 }
